51ad37a7cd272324275447eb54a9caad63a475ab
akshatpatel555 <akshatpatel555@gmail.com>
2021-10-21 18:11:36 -0700

    index.js
    took notes in class 10/21/21

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 console.log("hello world!")
 
 console.log("========================")
@@ -20,9 +19,8 @@ console.log("i =", i, "typeofi(i):", typeof(i))
 console.log("========================")
 console.log("numbers")
 console.log("========================")
-=======
-=======
->>>>>>> 25c0a71789f5b3e2b2e8e4334364506784b21982
+
+
 console.log("Hello, world!")
 
 console.log("====================")
@@ -41,16 +39,10 @@ console.log("== theVariable:", theVariable, "typeof(theVariable):", typeof(theVa
 console.log("====================")
 console.log("== Numbers")
 console.log("====================")
-<<<<<<< HEAD
->>>>>>> 25c0a71789f5b3e2b2e8e4334364506784b21982
-=======
->>>>>>> 25c0a71789f5b3e2b2e8e4334364506784b21982
 
 var n = 9
 var m = 5
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 console.log("n/m:", n/m)
 
 console.log("========================")
@@ -73,9 +65,7 @@ var str2 = 'This is string 2'
 var concatenation = str1 + str2
 console.log("adding str1 + str2", concatenation)
 console.log("adding str1 + 16:", str1 + 16)
-=======
-=======
->>>>>>> 25c0a71789f5b3e2b2e8e4334364506784b21982
+
 console.log("== n / m:", n / m)
 
 console.log("====================")
@@ -104,7 +94,71 @@ console.log("== str1[3]:", str1[3])
 console.log("== str1.replace('s', 'x'):", str1.replace('s', 'x'))
 console.log("== 'decathlon'.indexOf('cat'):", 'decathlon'.indexOf('cat'))
 console.log("== 'team'.indexOf('i'):", 'team'.indexOf('i'))
-<<<<<<< HEAD
->>>>>>> 25c0a71789f5b3e2b2e8e4334364506784b21982
-=======
->>>>>>> 25c0a71789f5b3e2b2e8e4334364506784b21982
+
+console.log("====================")
+console.log("== functions")
+console.log("====================")
+
+function addStuff(a, b, c){
+    console.log("== In addStuff, arguments: ", arguments)
+    return a + b + c
+}
+
+console.log("== addStuff(1, 2, 3): ", addStuff(1, 2, 3))
+console.log("== addStuff: ", addStuff('1', '2', '3'))
+console.log("== addStuff(1, 2, '3'): ", addStuff(1, 2, '3'))
+console.log("== typeof(addStuff: ", typeof(addStuff))
+console.log("== addStuff(1, 2): ", addStuff(1, 2))                      //c becomes undefined
+console.log("== addStuff(1, 2, 3, 4): ", addStuff(1, 2, 3, 4))          //can pass more values than in the function
+
+var foo = addStuff;
+console.log("== foo(1, 2, 3): ", foo(1, 2, 3))
+
+var bar = function () {                                                 //anonymous function
+    console.log("== Inside out anonymous function")
+}
+
+console.log("====================")
+console.log("== arrays")
+console.log("====================")
+
+var array = [1, "2", foo, ['a', 'b', 'c']]
+console.log("== array: ", array)
+console.log("== array.length: ", array.length)
+console.log
+
+for (var i = 0; i < array.length; i++){                                 //array loop
+    console.log("== array[" + 1 + "]:" , array[i])
+}
+
+array.forEach(function (elem) {                                         //forEach element in the array
+    console.log("== In forEach(), elem: ", elem)
+})
+
+console.log("====================")
+console.log("== object")
+console.log("====================")
+
+var obj = {}                                                             //mapping objects to their discriptions       
+console.log("== object: ", obj)                                          
+
+var mapping = {
+    'cat': 'cute',
+    'dog': 'loyal'
+}
+
+console.log("== mapping: ", mapping)
+console.log("== mapping ['cat']: ", mapping['cat'])
+
+var student = {
+    name: 'Luke Skywalker',
+    gpa: 3.14,
+    email: 'thelastjedi@gmail.com'
+}
+
+console.log("== student: ", student)
+console.log("== student['name']: ", student['name'])
+console.log("== student.name: ", student.name)
+
+var field = 'gpa'
+console.log("== student[field]: ", student[field])
